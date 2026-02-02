161425eb6524afc41a2bb3dfe0c2a6e62d1bad29
adwaid-p <adwaidpaloli007@gmail.com>
2026-02-02 10:07:07 -0500

    Sovle Practice Task

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,15 @@ function calculateArea() {
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
+let grocery1;
+let grocery2;
+let grocery3;
+
+function groceryTracker(){
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let totalAmount = grocery1 + grocery2 + grocery3;
+    document.getElementById('totalAmount').innerText = totalAmount; 
+}
